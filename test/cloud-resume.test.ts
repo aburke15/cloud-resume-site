baf9c96c44ef6811001f1d749083cb3d1d210365
Andre Burke <aburke.dev@gmail.com>
2022-11-12 00:42:18 -0700

    Initial commit

---
+++ test/cloud-resume.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudResume from '../lib/cloud-resume-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-resume-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudResume.CloudResumeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
